2194c03d5607136cef917d3d7abbb6636b9d0b6e
Niko Drossos <ndross427@west-mec.org>
2023-06-08 12:35:56 -0700

    Added file confirmation

--- File Encryption/Encryption/Encrypt.py
+++ File Encryption/Encryption/Encrypt.py
@@ -1,7 +1,7 @@
 import os
-import importlib.util
 import re
 import random
+import subprocess
 
 # Create dir "Keys" & "File Paths" if they do not already exist 
 def create_directory_if_not_exists(directory_path):
@@ -10,37 +10,50 @@ def create_directory_if_not_exists(directory_path):
 
 script_directory = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
+# Define key folder location
 key_files = os.path.join(script_directory, "File Encryption", "Decryption", "Keys")
 create_directory_if_not_exists(key_files)
 
+# Define file path folder location
 path_files = os.path.join(script_directory, "File Encryption", "Decryption", "File Paths")
 create_directory_if_not_exists(path_files)
 
+# Generate the key file name based on the random number
 def get_key_path(i):
-    # Generate the key file path based on the key number
-    key_path = os.path.join(key_files, f"thekey{i}.key")
+    key_path = os.path.join(key_files, f"theKey{i}.key")
     return key_path
 
-
+# Generate the file name for storing the encrypt file path
 def get_file_path(i):
-    # Generate the file path for storing the encrypt file path
     file_path = os.path.join(path_files, f"EncryptFilePath{i}.txt")
     return file_path
 
-
-def find_key_files(directory):
-    key_files = []
-    # Recursively search for key files in the given directory
-    for root, dirs, files in os.walk(directory):
+# Find all the key files inside of the "Keys" directory
+def find_key_files(keys_folder):
+    keys = []
+    for root, dirs, files in os.walk(keys_folder):
         for file in files:
-            # Check if the file starts with "thekey" and ends with ".key"
-            if file.startswith("thekey") and file.endswith(".key"):
-                key_files.append(os.path.join(root, file))
-    return key_files
+            # Check if the file starts with "theKey" and ends with ".key"
+            if file.startswith("theKey") and file.endswith(".key"):
+                keys.append(os.path.join(root, file))
+    return keys
 
+# Ask the user to confirm if it is the correct folder they want encrypted
+def confirm_choice(directory):
+    confirm = input(f'Confirm "{directory}" is folder you want encrypted: (y / n): ')
+    if confirm == "n":
+        print("Stopping")
+        return False
+    elif confirm == "y":
+        print("Selected Folder:", directory)
+        return True
+    else:
+        print("Invalid input try again")
+        return confirm_choice(directory)
 
+# Extract the last digits from the filename
 def extract_last_digits(filename):
-    # Extract the last digits from the filename
+    # Uses regex to find all the numbers at the end of the filename string
     match = re.search(r"\d+", filename[::-1])
     if match:
         return int(match.group()[::-1])
@@ -56,9 +69,11 @@ def is_folder_encrypted(directory):
         with open(file_path, "r") as file_contents:
             encrypted_paths.append(file_contents.read())
 
-    # Check if any parent directory of the given directory is already encrypted
+    # Check if any parent or child directory of the given directory is already encrypted
     for encrypted_path in encrypted_paths:
         if directory.startswith(encrypted_path):
+            return True
+        elif encrypted_path.startswith(directory):
             return True
     return False
 
@@ -66,27 +81,21 @@ def is_folder_encrypted(directory):
 def main():
     # Check if the cryptography module is installed
     try:
-        importlib.util.find_spec("cryptography")
         from cryptography.fernet import Fernet
     except ImportError:
-        import subprocess
         subprocess.run(["pip", "install", "cryptography"])
         from cryptography.fernet import Fernet
 
+    # Check if tKinter is already installed
     try:
-        importlib.util.find_spec("tkinter")
         import tkinter as tk
         from tkinter import filedialog
     except ImportError:
-        import subprocess
         subprocess.run(["pip", "install", "tkinter"])
         import tkinter as tk
         from tkinter import filedialog
 
-    # Import Fernet after ensuring cryptography is installed
-    from cryptography.fernet import Fernet
-
-    # Generate a random number to append to key and file name
+    # Generate a random number to append to key and file name to pair them up
     i = random.randint(100000, 999999)
     Key_Path = get_key_path(i)
     File_Path = get_file_path(i)
@@ -104,8 +113,14 @@ def main():
         root.destroy()
         return
     
+    # Make the directory path look cleaner 
+    directory = os.path.normpath(directory)
+
+    if not confirm_choice(directory):
+        return
+
     # Prevent the user from encrypting the script and locking all the previously encrypted files 
-    cant_encrypt = os.path.normpath(os.path.normcase(directory).title())
+    cant_encrypt = os.path.normcase(directory).title()
     encrypt_protection = cant_encrypt[0].lower() + cant_encrypt[1:]
 
     # Prevents you from encrypting the script file or any of its children
@@ -115,13 +130,13 @@ def main():
         input("Chosen directory is the same as the script directory or its child. You cannot encrypt this software")
         return
 
+    # Change directory to selected one and print it to the stdout
     os.chdir(directory)
-    print("Selected Folder:", directory)
     root.destroy()  # Close the window after selecting a folder
 
     # Run a test to check if the folder is already encrypted
     if is_folder_encrypted(directory):
-        input(f"Folder {directory} is already encrypted. \nPress enter to exit...")
+        input(f'Folder "{directory}" already contains encrypted files. \nPress enter to exit...')
         return
 
     # Function to encrypt the contents of a file
@@ -152,10 +167,6 @@ def main():
             # Write the encryption key
             with open(Key_Path, "wb") as key_file:
                 key_file.write(key)
-
-            # Read the key file
-            with open(Key_Path, "rb") as key_file:
-                key = key_file.read()
 
             # Read the path file
             with open(File_Path, "w") as path_file:
